ff412203e5d60d50b8314d22bbf851409d91fbd1
Felipe Rooke <felipe@rooke.com.br>
2018-05-28 10:35:15 -0300

    Criando array de minutos

--- V2/processa_minutos.py
+++ V2/processa_minutos.py
@@ -18,55 +18,13 @@ import numpy as np
 import datetime as dt
 import pytz
 
-df = pd.read_csv('data/Header_NaN_True_Filtered_ModoApi_Data.csv',
-                 names=["LocationID", "CarID", "CaptureTime", "StartTime"])
-df.count()
-# limpa registros com nulo
-df = df.dropna()
-
-df.dtypes
-
 types = {
     'LocationID': int,
     'CarID': int,
-    'StartTime': int}
+    'StartTime': int,
+    'CaptureTime': int}
 
-df = df.astype(dtype=types)
-
-df.count()
-
-df
-
-df['CaptureTime'] = pd.to_datetime(df['CaptureTime'])
-
-
-df['CaptureTime'] = df['CaptureTime'].astype('int64')//1e9
-
-
-df.to_csv('data/Timestemp_Header_NaN_True_Filtered_ModoApi_Data.csv', index=False)
-
-df
-
-# Dom, 5 de Nov, 02:00 - 1509854400
-# Dom, 15 de Out, 00:00 - 1508036400
-
-df[df.CaptureTime < 1508036400].count()
-df[(df.CaptureTime > 1508036400) & (df.CaptureTime < 1509854400)].count()
-df[df.CaptureTime > 1509854400].count()
-
-# ate dia 15/10 diminui 4h
-df.loc[df.CaptureTime < 1508036400, "CaptureTime"] = df["CaptureTime"] - (4*3600)
-
-# do dia 15/10 até dia 05/11 diminui 5h
-df.loc[(df.CaptureTime > 1508036400) & (df.CaptureTime < 1509854400),
-       "CaptureTime"] = df["CaptureTime"] - (5*3600)
-
-# do dia 05/11 pra frente diminui 6h
-df.loc[df.CaptureTime > 1509854400, "CaptureTime"] = df["CaptureTime"] - (6*3600)
-
-df.to_csv('data/Vancouver_Timestemp_Header_NaN_True_Filtered_ModoApi_Data.csv', index=False)
-
-df
+df = pd.read_csv('data/Vancouver_Timestemp_Header_NaN_True_Filtered_ModoApi_Data.csv', dtype=types)
 
 # ordena pelo carro, hora de captura e start
 df.sort_values(by=['CarID', 'CaptureTime', 'StartTime'], inplace=True)
